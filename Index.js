01a63f8813e54d9d3ad02f7a5207195c6d13c74d
Vasu Aggarwal <svasu.sv95@gmail.com>
2023-02-25 14:14:15 +0530

    added files

---
+++ Index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const port = 8000;
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Home page of the api testing.")
+})
+
+app.get("/api/test", (req, res) => {
+    res.send("api testing kr raha hu yr")
+})
+
+app.get("/api/test/json", (req, res) => {
+    res.json({
+        "Name": "Vasu Aggarwal", 
+        "Age": "22",
+        "Work": "Api Testing for Android Application"})
+});
+
+app.listen(port, () => {
+    console.log("Node server is running on the port: ", port);
+});
