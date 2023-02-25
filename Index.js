c15d33d9fdfa66b8e57bece1192cbf39aedadea0
Vasu Aggarwal <svasu.sv95@gmail.com>
2023-02-25 14:38:14 +0530

    added cors requirement

--- Index.js
+++ Index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const port = 8000;
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors);
 
 app.get("/", (req, res) => {
     res.send("Home page of the api testing.")
